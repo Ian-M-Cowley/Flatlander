c4f23941572e81ab0b469de1efddd8496a9c53bc
Ian Cowley <ian@planoly.com>
2018-02-15 19:46:12 -0600

    Fix issue in favoriting

--- app/src/main/java/com/flatlander/flatlander/data/LocalFavoritesRepository.kt
+++ app/src/main/java/com/flatlander/flatlander/data/LocalFavoritesRepository.kt
@@ -1,8 +1,8 @@
 package com.flatlander.flatlander.data
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import com.flatlander.flatlander.model.Site
 import com.flatlander.flatlander.model.SiteLite
 import com.google.gson.Gson
 import io.reactivex.Single
@@ -28,25 +28,27 @@ class LocalFavoritesRepository : FavoritesRepository {
         this.sharedPrefs = context.getSharedPreferences("Favorites", Context.MODE_PRIVATE)
     }
 
+    @SuppressLint("ApplySharedPref")
     override fun favoriteSite(siteLite: SiteLite): Single<Boolean> {
         val ids = sharedPrefs.getStringSet(PREF_FAVORITE_IDS, mutableSetOf())
         ids.add(siteLite.getUniqueId())
-        sharedPrefs.edit().putStringSet(PREF_FAVORITE_IDS, ids).apply()
+        sharedPrefs.edit().putStringSet(PREF_FAVORITE_IDS, ids).commit()
 
         val sites = sharedPrefs.getStringSet(PREF_FAVORITE_SITES, mutableSetOf())
         sites.add(Gson().toJson(siteLite))
-        sharedPrefs.edit().putStringSet(PREF_FAVORITE_SITES, sites).apply()
+        sharedPrefs.edit().putStringSet(PREF_FAVORITE_SITES, sites).commit()
         return Single.just(true)
     }
 
+    @SuppressLint("ApplySharedPref")
     override fun unfavoriteSite(siteLite: SiteLite): Single<Boolean> {
         val ids = sharedPrefs.getStringSet(PREF_FAVORITE_IDS, mutableSetOf())
         ids.remove(siteLite.getUniqueId())
-        sharedPrefs.edit().putStringSet(PREF_FAVORITE_IDS, ids).apply()
+        sharedPrefs.edit().putStringSet(PREF_FAVORITE_IDS, ids).commit()
 
         val sites = sharedPrefs.getStringSet(PREF_FAVORITE_SITES, mutableSetOf())
         sites.remove(Gson().toJson(siteLite))
-        sharedPrefs.edit().putStringSet(PREF_FAVORITE_SITES, sites).apply()
+        sharedPrefs.edit().putStringSet(PREF_FAVORITE_SITES, sites).commit()
         return Single.just(true)
     }
 
@@ -60,7 +62,7 @@ class LocalFavoritesRepository : FavoritesRepository {
 
         val gson = Gson()
         sitesSet.iterator().forEach {
-            val site = gson.fromJson<Site>(it, Site::class.java)
+            val site = gson.fromJson<SiteLite>(it, SiteLite::class.java)
             sitesList.add(site)
         }
         return Single.just(sitesList)
