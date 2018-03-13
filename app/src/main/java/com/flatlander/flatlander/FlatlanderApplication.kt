380ed70087eb699b4e2f0e8d2253f05922341217
Ian Cowley <ian@planoly.com>
2018-03-12 22:09:52 -0500

    Only enable Crashlytics on non Debug builds.

--- app/src/main/java/com/flatlander/flatlander/FlatlanderApplication.kt
+++ app/src/main/java/com/flatlander/flatlander/FlatlanderApplication.kt
@@ -1,8 +1,10 @@
 package com.flatlander.flatlander
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.flatlander.flatlander.data.LocalFavoritesRepository
 import com.flatlander.flatlander.data.SharedPrefs
+import io.fabric.sdk.android.Fabric
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
 
@@ -16,5 +18,9 @@ class FlatlanderApplication : Application() {
         LocalFavoritesRepository.instance.init(this)
         SharedPrefs.instance.init(this)
         CalligraphyConfig.initDefault(CalligraphyConfig.get())
+
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, Crashlytics())
+        }
     }
 }
